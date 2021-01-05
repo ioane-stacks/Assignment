08dfcb30d5c8c9644d3cbf046896a6c94a3c7f1c
Ioane <38262019+ioane-stacks@users.noreply.github.com>
2021-01-05 20:49:43 +0400

    Update assignment.cs

--- assignment.cs
+++ assignment.cs
@@ -1,1 +1,26 @@
 
+// 0 //
+static bool isPalindrome(string text)
+        {
+            char[] charArray = new char[text.Length];
+            for (int i = 0; i < text.Length; i++) charArray[i] = text[i];
+
+            Array.Reverse(charArray);
+
+            string temp = new string(charArray);
+
+            Console.WriteLine(temp.Equals(text));
+            return temp.Equals(text);
+
+        }//
+static void Main(string[] args)
+        {
+
+            Console.Write("Name to check pallindrome: ");
+            isPalindrome(Console.ReadLine());
+
+            Console.ReadKey();
+        }    
+// 0 //
+
+// 1 //
