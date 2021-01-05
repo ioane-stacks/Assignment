230be875202268b67a5c0b549a20e983403d06a0
Ioane <38262019+ioane-stacks@users.noreply.github.com>
2021-01-05 22:10:11 +0400

    Update assignment.cs

--- assignment.cs
+++ assignment.cs
@@ -22,3 +22,37 @@ static void Main(string[] args)
 // 0 //
 
 // 1 //
+static int minSplit(int amount)
+        {
+            int[] monets = { 1, 5, 10, 20, 50 };
+            List<int> quantity = new List<int> { };
+
+            int maxMonet = 0;
+
+            for (int i = 0; i < monets.Length; i++)
+            {
+                if (amount / monets[i] > 0)
+                {
+                    quantity.Add(amount / monets[i]);
+                    maxMonet = monets[i];
+
+                    Console.WriteLine($"\n{monets[i]} tetri: {amount / monets[i]} - cali");
+
+                }
+            }
+
+            Console.WriteLine($"Dasabrunebeli monetis minimaluri raodenoba: {quantity.Min()} cali - {maxMonet} tetri");
+
+            return quantity.Min();
+        }
+
+static void Main(string[] args)
+        {
+            Console.Write("Shemoitanet tanxa tetrebshi: ");
+            minSplit(int.Parse(Console.ReadLine()));
+
+
+
+            Console.ReadKey();
+        }
+// 1 //
