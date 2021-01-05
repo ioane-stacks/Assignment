d456dd26bba6c43db92ea5e2919939b6730f9e6d
Ioane <38262019+ioane-stacks@users.noreply.github.com>
2021-01-05 21:25:04 +0400

    Update assignment.cs

--- assignment.cs
+++ assignment.cs
@@ -4,14 +4,12 @@ static bool isPalindrome(string text)
         {
             char[] charArray = new char[text.Length];
             for (int i = 0; i < text.Length; i++) charArray[i] = text[i];
-
+        
             Array.Reverse(charArray);
-
             string temp = new string(charArray);
-
+        
             Console.WriteLine(temp.Equals(text));
             return temp.Equals(text);
-
         }
 static void Main(string[] args)
         {
